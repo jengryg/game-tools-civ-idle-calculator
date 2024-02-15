ea4fd323ba9e6498f849d6b94afe90916e11976d
jengryg <62914315+jengryg@users.noreply.github.com>
2024-02-15 22:46:57 +0100

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    jacoco
+    application
+    idea
+}
+
+group = "game.tools"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.slf4j:slf4j-api:2.0.10")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.16.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.16.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testImplementation("org.assertj:assertj-core:3.25.2")
+    testImplementation("io.mockk:mockk:1.13.9")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+idea {
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+import ch.qos.logback.classic.Level
+
+fun main(args: Array<String>) {
+    setLoggingLevel(Level.TRACE)
+}
+
