9dedb01b1303d0270432222655323c644c3555ff
jengryg <62914315+jengryg@users.noreply.github.com>
2024-05-30 21:11:20 +0200

    Change simulation settings for current play style.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,11 +6,11 @@ import analysis.defs.WonderPriceListExporter
 import analysis.strategy.SimplexStrategyFactory
 import analysis.visual.CurrentMapVisualizer
 import ch.qos.logback.classic.Level
-import constants.SPACECRAFT_FACTORY
-import game.common.BuildingType
+import constants.*
+import game.common.modifiers.BuildingModTarget
+import game.common.modifiers.BuildingModType
 import game.loader.DataLoader
 import game.model.ModelFactory
-import game.model.game.Resource
 
 fun main(args: Array<String>) {
     setLoggingLevel(args.toList().getOrElse(0) { Level.WARN.levelStr })
@@ -29,18 +29,51 @@ fun main(args: Array<String>) {
         export()
     }
 
-    val ssf = SimplexStrategyFactory(model)
-    val targetEnterprise = 2_000_000_000.0 / 20.0
+    model.getBuilding(SATELLITE_FACTORY).addCustomMod(
+        type = BuildingModType.WONDER,
+        target = BuildingModTarget.OUTPUT,
+        from = APOLLO_PROGRAM,
+        totalEffect = 1.0
+    )
 
-    model.buildings.filterValues { it.type == BuildingType.NORMAL }.forEach { (name, building) ->
-        val r = Resource.getEvOf(building.effectiveOutput)
-        if (r > 0.0) {
-            ssf.processByEnterpriseValueTarget(name, targetEnterprise).apply {
-                solve()
-                export()
-            }
-        }
+//    listOf(BuildingModTarget.OUTPUT, BuildingModTarget.STORAGE, BuildingModTarget.WORKER).forEach { target ->
+//        listOf(
+//            STEEL_MILL,
+//            FRIGATE_BUILDER,
+//            SPACECRAFT_FACTORY,
+//            BATTLESHIP_BUILDER,
+//            BLACKSMITH,
+//            PUBLISHING_HOUSE
+//        ).forEach { bld ->
+//            model.getBuilding(bld).addCustomMod(
+//                type = BuildingModType.WONDER,
+//                target = target,
+//                from = UNITED_NATIONS,
+//                totalEffect = 5.0
+//            )
+//        }
+//    }
+
+    listOf(SPACECRAFT_FACTORY, SATELLITE_FACTORY).forEach {
+        model.getBuilding(it).addCustomMod(
+            type = BuildingModType.GREAT_PERSON,
+            target = BuildingModTarget.OUTPUT,
+            from = "Doubled GP",
+            totalEffect = model.getObtainedGreatPeople("SergeiKorolev").totalEffect
+        )
     }
+
+    val ssf = SimplexStrategyFactory(model)
+
+//    model.buildings.filterValues { it.type == BuildingType.NORMAL }.forEach { (name, building) ->
+//        val r = Resource.getEvOf(building.effectiveOutput)
+//        if (r > 0.0) {
+//            ssf.processByEnterpriseValueTarget(name, targetEnterprise).apply {
+//                solve()
+//                export()
+//            }
+//        }
+//    }
 
     ssf.process(SPACECRAFT_FACTORY, 200.0).apply {
         solve()
